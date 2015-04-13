25371da69ed226dddf3a4fe746012d903368e894
Julian Simioni <julian@simioni.org>
2015-04-13 15:37:21 +0200

    Fix order of ids in test

--- src/test/kdtree/SimpleTreeBuilderTest.h
+++ src/test/kdtree/SimpleTreeBuilderTest.h
@@ -132,7 +132,7 @@ namespace KDTree {
 				items.push_back(Item<int, 2>(1, Vector2d(2, 3)));
 				items.push_back(Item<int, 2>(2, Vector2d(3, 3)));
 				items.push_back(Item<int, 2>(3, Vector2d(4, 3)));
-				items.push_back(Item<int, 2>(5, Vector2d(1, 3)));
+				items.push_back(Item<int, 2>(4, Vector2d(1, 3)));
 				PlaneSplitNode<int, 2>* result = builder.build_plane_split(items, 3);
 				TEST_ASSERT(result);
 				if (result) {
@@ -143,7 +143,7 @@ namespace KDTree {
 					PlaneSplitNode<int, 2>* left = result->getLeft();
 					if (left) {
 						TEST_ASSERT(left->getItems().size() == 1);
-						TEST_ASSERT(left->getItems().begin()->item == 5);
+						TEST_ASSERT(left->getItems().begin()->item == 4);
 					}
 					TEST_ASSERT(result->getRight());
 					PlaneSplitNode<int, 2>* right = result->getRight();
